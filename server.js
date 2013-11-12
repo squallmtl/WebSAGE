698f2ee0a9ea969418cf6d7d98685bceb9f34d23
jillian.aurisano <jillian.aurisano@gmail.com>
2013-11-12 12:27:26 -0600

    about to merge in new omicronjs code

--- server.js
+++ server.js
@@ -38,9 +38,9 @@ sio.configure('development', function () {
 var initDate = new Date();
 
 var fs = require('fs');
-//var file = 'config/desktop-cfg.json';
+var file = 'config/desktop-cfg.json';
 //var file = 'config/thor-cfg.json';
-var file = 'config/iridium-cfg.json';
+//var file = 'config/iridium-cfg.json';
 
 var config;
 fs.readFile(file, 'utf8', function(err, json_str) {
@@ -66,17 +66,23 @@ sio.sockets.on('connection', function(socket) {
 	socket.emit('setSystemTime', cDate.getTime()-initDate.getTime());
 	socket.emit('setupDisplayConfiguration', config);
 
-	/* adding new elements */
-	socket.emit('addNewElement', {type: "img", id: "item1", src: "images/sage_logo.jpg"});
-	socket.emit('addNewElement', {type: "img", id: "item2", src: "images/evl-logo-small.jpg"});
-	socket.emit('addNewElement', {type: "img", id: "item3", src: "images/omegalib-black.png"});
-	
-	/* jillian's elements */
-	//socket.emit('addNewElement', {type: "site", id: "keggPathway", src: "protovisExample.html", width: 1000, height: 800 });
-	//socket.emit('addNewElement', {type: "site", id: "keggPathway", src: "http://www.gmail.com", width: 500, height: 400 });
-	//socket.emit('addNewElement', {type: "site", id: "keggPathway", src: "http://webglmol.sourceforge.jp/glmol/viewer.html", width: 1000, height: 800 });
-    socket.emit('addNewElement',  {type: "site", id: "webglExample", src: "http://webglsamples.googlecode.com/hg/blob/blob.html", width: 1000, height: 800 });
-
+//	/* adding new elements */
+//	socket.emit('addNewElement', {type: "img", id: "item1", src: "images/sage_logo.jpg"});
+//	socket.emit('addNewElement', {type: "img", id: "item2", src: "images/evl-logo-small.jpg"});
+//	socket.emit('addNewElement', {type: "img", id: "item3", src: "images/omegalib-black.png"});
+//	
+//	/* jillian's elements */
+//	//socket.emit('addNewElement', {type: "site", id: "keggPathway", src: "protovisExample.html", width: 1000, height: 800 });
+//	//socket.emit('addNewElement', {type: "site", id: "keggPathway", src: "http://www.gmail.com", width: 500, height: 400 });
+//	//socket.emit('addNewElement', {type: "site", id: "keggPathway", src: "http://webglmol.sourceforge.jp/glmol/viewer.html", width: 1000, height: 800 });
+//    socket.emit('addNewElement',  {type: "site", id: "webglExample1", src: "http://webglsamples.googlecode.com/hg/blob/blob.html", width: 1000, height: 800 });
+////    socket.emit('addNewElement',  {type: "site", id: "webglExample2", src: "http://threejs.org/examples/#webgl_animation_skinning_morph", width: 1000, height: 800 });  //Good test of synchronization
+//
+               
+               
+   /* test pointer */
+   socket.emit('createPointer', {type: 'ptr', id: '0', src: "resources/mouse-pointer-hi.png" });
+               
 	for(i=0; i<items.length; i++){
 		socket.emit('addNewElement', items[i]);
 	}
@@ -160,6 +166,123 @@ app.post('/upload', function(request, response) {
 	
 	response.end("upload complete");
 });
+
+
+
+// ---------------------------------------------
+// DATA FROM OMICRONJS
+// ADAPTED FROM LUC's omicronjs server code
+// ---------------------------------------------
+
+var net = require('net');
+var util = require('util');
+var dgram = require('dgram');
+var sprint = require('sprint').sprint;
+
+// Global UDP socket to the tracker server
+var udp = undefined;
+
+var tserver   = "omgtracker.evl.uic.edu";
+var tport     = 28000;
+var pdataPort = 9123;
+var client    = net.connect(tport, tserver,  function() { //'connect' listener
+                            console.log('client connected: ', tserver, tport);
+                            
+                    var sendbuf = util.format("data_on,%d", pdataPort);
+                    console.log("OMicron> Sending handshake: ", sendbuf);
+                    client.write(sendbuf);
+                    
+                    udp = dgram.createSocket("udp4");
+                    var dstart = Date.now();
+                    var emit = 0;
+                    udp.on("message", function (msg, rinfo) {
+                           // console.log("UDP> got: " + msg + " from " + rinfo.address + ":" + rinfo.port);
+                           // var out = util.format("UDP> msg from [%s:%d] %d bytes", rinfo.address,rinfo.port,msg.length);
+                           // console.log(out);
+                           
+                           if ((Date.now() - dstart) > 100) {
+                           var offset = 0;
+                           var e = {};
+                           if (offset < msg.length) e.timestamp = msg.readUInt32LE(offset); offset += 4;
+                           if (offset < msg.length) e.sourceId = msg.readUInt32LE(offset); offset += 4;//wandId, touchId, ptrId -- unique id for particular object
+                           if (offset < msg.length) e.serviceId = msg.readInt32LE(offset); offset += 4; //denote is it touch, sage ptr or mouse
+                           if (offset < msg.length) e.serviceType = msg.readUInt32LE(offset); offset += 4;//ptr, mocap, wand
+                           if (offset < msg.length) e.type = msg.readUInt32LE(offset); offset += 4;//event type: click, drag, scroll
+                           if (offset < msg.length) e.flags = msg.readUInt32LE(offset); offset += 4;//button flags
+                           
+                           if (offset < msg.length) e.posx = msg.readFloatLE(offset); offset += 4;//0-1 convert to screen dimensions
+                           if (offset < msg.length) e.posy = msg.readFloatLE(offset); offset += 4;
+                           if (offset < msg.length) e.posz = msg.readFloatLE(offset); offset += 4;
+                           if (offset < msg.length) e.orw = msg.readFloatLE(offset); offset += 4; //none for ptr, but for wand
+                           if (offset < msg.length) e.orx = msg.readFloatLE(offset); offset += 4;
+                           if (offset < msg.length) e.ory = msg.readFloatLE(offset); offset += 4;
+                           if (offset < msg.length) e.orz = msg.readFloatLE(offset); offset += 4;
+                           if (offset < msg.length) e.extraDataType = msg.readUInt32LE(offset); offset += 4;  //eventually store string vals for ptr: color, ip address
+                           if (offset < msg.length) e.extraDataItems = msg.readUInt32LE(offset); offset += 4; 
+                           if (offset < msg.length) e.extraDataMask = msg.readUInt32LE(offset); offset += 4;
+                           // memcpy(ed.extraData, &eventPacket[offset], EventData::ExtraDataSize);
+                           
+                           var r_roll  = Math.asin(2.0*e.orx*e.ory + 2.0*e.orz*e.orw);
+                           var r_yaw   = Math.atan2(2.0*e.ory*e.orw-2.0*e.orx*e.orz , 1.0 - 2.0*e.ory*e.ory - 2.0*e.orz*e.orz);
+                           var r_pitch = Math.atan2(2.0*e.orx*e.orw-2.0*e.ory*e.orz , 1.0 - 2.0*e.orx*e.orx - 2.0*e.orz*e.orz);
+                           
+                           
+//                           if (e.type == 3) {//  events
+//                               if (e.serviceType == 1) {  //ptr
+//                                   var RAD_TO_DEGREE = 180.0/Math.PI;
+//                                   var pfmt = sprint("\tp: x %6.2fm | y %6.2fm | z %6.2fm\n",
+//                                                     e.posx, e.posy, e.posz); // in meters
+//                                   var rfmt = sprint("\tr: p(x) %4.0f | y(y) %4.0f | r(z) %4.0f\n", // degrees
+//                                                     RAD_TO_DEGREE * r_pitch, RAD_TO_DEGREE * r_yaw, RAD_TO_DEGREE * r_roll);
+//                                   
+//                                   if (e.sourceId == 0) {
+//                                       //console.log("head> ", pfmt+rfmt);
+//                                       sio.sockets.emit('head', {text: pfmt+rfmt, pos:[e.posx, e.posy, e.posz],
+//                                                        rot:[RAD_TO_DEGREE * r_pitch, RAD_TO_DEGREE * r_yaw, RAD_TO_DEGREE * r_roll]});
+//                                   }
+//                                   if (e.sourceId == 1) {
+//                                       //console.log("wand> ", pfmt+rfmt);
+//                                       sio.sockets.emit('wand', {text: pfmt+rfmt, pos:[e.posx, e.posy, e.posz],
+//                                                        rot:[RAD_TO_DEGREE * r_pitch, RAD_TO_DEGREE * r_yaw, RAD_TO_DEGREE * r_roll]});
+//                                   }
+//                                   if (e.sourceId == 2) {
+//                                       //console.log("wand2> ", pfmt+rfmt);
+//                                       sio.sockets.emit('wand2', {text: pfmt+rfmt, pos:[e.posx, e.posy, e.posz],
+//                                                        rot:[RAD_TO_DEGREE * r_pitch, RAD_TO_DEGREE * r_yaw, RAD_TO_DEGREE * r_roll]});
+//                                   }
+//                                   emit++;
+//                               }
+//                           }
+                           if( e.type == 4 ) //MOVE event is of type 4
+                           {
+                               if( e.serviceType == 0 ) //SAGE pointer event
+                               {
+                                   var pfmt = sprint("\tp: x %6.2fm | y %6.2fm | z %6.2fm\n",
+                                                     e.posx, e.posy, e.posz); // in meters
+                                   console.log( "pointer moved: " + e.posx + " " + e.posy);
+//                           
+//                              sio.sockets.emit('createPointer', {type: 'ptr', id: '1', src: "resources/cursor_arrow_48x48.png" });
+//                           sio.sockets.emit('movePointer', "hi");
+
+                           
+                                   sio.sockets.emit( 'movePointer',{elemId: '0', elemLeft: e.posx, elemTop: e.posy});
+                           
+                           
+//                                    sio.sockets.emit('TEST', "hi");
+                               }
+                           }
+                           
+                           if (emit>2) { dstart = Date.now(); emit = 0; }
+                           }
+                           });
+                            
+                            udp.on("listening", function () {
+                                   var address = udp.address();
+                                   console.log("UDP> listening " + address.address + ":" + address.port);
+                                   });
+                            
+                            udp.bind(pdataPort);
+                            });
 
 /////////////////////////////////////////////////////////////////////////
 
