e8677911953d43029400c47c985794888813fd0c
tmarrinan <thomas.j.marrinan@gmail.com>
2013-12-11 11:24:10 -0600

    replaced node_modules with packages.json

--- server.js
+++ server.js
@@ -45,10 +45,10 @@ sio.configure('development', function () {
 
 var initDate = new Date();
 
-//var file = 'config/desktop-cfg.json';
+var file = 'config/desktop-cfg.json';
 //var file = 'config/thor-cfg.json';
 //var file = 'config/iridium-cfg.json';
-var file = 'config/iridiumX-cfg.json';
+//var file = 'config/iridiumX-cfg.json';
 //var file = 'config/lyra-cfg.json';
 var config;
 fs.readFile(file, 'utf8', function(err, json_str) {
