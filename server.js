1b35426d8c9beece01c4304d9a3cdfb34f582e93
Thomas Marrinan <thomas.j.marrinan@gmail.com>
2014-02-28 13:58:13 -0600

    fixed thor scripts

--- server.js
+++ server.js
@@ -17,13 +17,13 @@ var sagepointer = require('node-sagepointer');         // custom node module
 
 // CONFIG FILE
 var wallfile = null;
-var wallfile = "config/tmarrinan-cfg.json";
+//var wallfile = "config/tmarrinan-cfg.json";
 //var wallfile = "config/desktop-cfg.json";
 //var wallfile = "config/desktop-omicron-cfg.json";
 //var wallfile = "config/icewall-cfg.json";
 //var wallfile = "config/icewallKB-cfg.json";
 //var wallfile = "config/icewallJA-cfg.json";
-//var wallfile = "config/icewallTM-cfg.json";
+var wallfile = "config/icewallTM-cfg.json";
 //var wallfile = "config/icewallAN-cfg.json";
 //var wallfile = "config/icewallRight-omicron-cfg.json";
 //var wallfile = "config/iridium-cfg.json";
