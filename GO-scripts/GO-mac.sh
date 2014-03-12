70058830b232cbc640c03406d6015fdbe4156299
Thomas Marrinan <thomas.j.marrinan@gmail.com>
2014-03-12 12:07:22 -0500

    fixed audio bugs for video playback

--- GO-scripts/GO-mac.sh
+++ GO-scripts/GO-mac.sh
@@ -1,21 +1,5 @@
 #!/bin/sh
-osascript <<END 
-tell application "Terminal"
-    do script "cd \"`pwd`\";cd ..;node server.js;exit"
-end tell
-END
-
-echo ""
-echo ""
 sleep 1
-
-#global_param="--new-window --window-size=1920,1080 --kiosk --disable-popup-blocking --nfirst-run --use-gl --enable-accelerated-compositing --allow-file-access-from-files"
-
-#UDD=$HOME/.config/chrome-nfs/mac0
-#mkdir -p $UDD/Default
-#param="$global_param --user-data-dir=$UDD"
-#/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome $param 'https://localhost:9090?clientID=0' &
-
 
 global_param="--new-window --window-size=700,700 --disable-popup-blocking --nfirst-run --use-gl --enable-accelerated-compositing --allow-file-access-from-files"
 
